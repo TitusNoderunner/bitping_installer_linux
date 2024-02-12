2f78294bf7dae9372f55120cfa826cf1eb4db5a3
TitusNoderunner <159756192+TitusNoderunner@users.noreply.github.com>
2024-02-12 12:42:21 +0400

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
+# Update and upgrade system packages
+sudo apt update && sudo apt upgrade -y
+
+# Install Docker, Docker Compose, and Podman
+sudo apt install docker.io docker-compose -y
+sudo apt install podman-docker -y
+
 # Install Bitping
 curl https://bitping.com/install.sh | bash
 
 # Check if bitpingd executable exists
-if [ -x "$(command -v bitpingd)" ]; then
+if [ -x "$(command -v /root/.local/bin/bitpingd)" ]; then
   echo "Bitping installed successfully."
 
   # List files in current directory
   ls
 
-  # Prompt user to enter email and password
-  read -p "Enter your Bitping email: " email
-  read -s -p "Enter your Bitping password: " password
-  echo
-
   # Login to Bitping
   echo "Logging in to Bitping..."
-  ./bitpingd login
+  /root/.local/bin/bitpingd login
 
   # Install Bitping service
   echo "Installing Bitping service..."
-  sudo ./bitpingd service install
+  sudo /root/.local/bin/bitpingd service install
 
   # Login to Bitping with email and password
   echo "Logging in to Bitping with email and password..."
-  sudo ./bitpingd login --email "$email" --password "$password"
+  sudo /root/.local/bin/bitpingd login --email "YOUR_BITPING_EMAIL" --password "YOUR_BITPING_PASSWORD"
 else
   echo "Error: Bitping not installed."
 fi
