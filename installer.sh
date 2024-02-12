523b2e81306a8fd8f288c64ff99d8dd7358c7b52
TitusNoderunner <159756192+TitusNoderunner@users.noreply.github.com>
2024-02-12 14:18:55 +0400

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -11,23 +11,28 @@ sudo apt install podman-docker -y
 curl https://bitping.com/install.sh | bash
 
 # Check if bitpingd executable exists
-if [ -x "$(command -v /root/.local/bin/bitpingd)" ]; then
+if [ -x "$(command -v bitpingd)" ]; then
   echo "Bitping installed successfully."
-
-  # List files in current directory
-  ls
 
   # Login to Bitping
   echo "Logging in to Bitping..."
-  /root/.local/bin/bitpingd login
+  bitpingd login
 
   # Install Bitping service
   echo "Installing Bitping service..."
-  sudo /root/.local/bin/bitpingd service install
+  sudo bitpingd service install
+
+  # Start Bitping service
+  echo "Starting Bitping service..."
+  sudo systemctl start bitpingd
+
+  # Enable Bitping service to start on boot
+  echo "Enabling Bitping service to start on boot..."
+  sudo systemctl enable bitpingd
 
   # Login to Bitping with email and password
   echo "Logging in to Bitping with email and password..."
-  sudo /root/.local/bin/bitpingd login --email "YOUR_BITPING_EMAIL" --password "YOUR_BITPING_PASSWORD"
+  sudo bitpingd login --email "YOUR_BITPING_EMAIL" --password "YOUR_BITPING_PASSWORD"
 else
   echo "Error: Bitping not installed."
 fi
